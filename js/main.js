18af1c23f940fb3980d7365050e84df298895ad0
aeibergen <aeibergen@wisc.edu>
2017-02-13 16:37:40 -0600

    Changed from leaflet graphic to point symbols

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 //Andrea Eibergen
+//February 7, 2017
+
+//GOAL: Proportional symbols representing attribute values of mapped features
+//STEPS:
+//1. Create the Leaflet map--done (in createMap())
+//2. Import GeoJSON data--done (in getData())
+//3. Add circle markers for point features to the map--done (in AJAX callback)
+//4. Determine which attribute to visualize with proportional symbols
+//5. For each feature, determine its value for the selected attribute
+//6. Give each feature's circle marker a radius based on its attribute value
 
 //function to create the Leaflet map
 function createMap(){
@@ -54,21 +64,6 @@ function onEachFeature(feature, layer) {
         }
         layer.bindPopup(popupContent);
     };
-};
-
-//define function to retrieve the data and place it on the map
-function getData(map){
-    //load the data
-    $.ajax("data/InternetUsage.geojson", {
-        dataType: "json",
-        success: function(response){
-
-            //create a Leaflet GeoJSON layer and add it to the map
-            L.geoJson(response, {
-                onEachFeature: onEachFeature
-            }).addTo(map);
-        }
-    });
 };
 
 $(document).ready(createMap);
