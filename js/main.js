e162d2a61069522daa458ca77b4b819011fbe3bf
aeibergen <aeibergen@wisc.edu>
2017-02-13 20:26:56 -0600

    Started Module 5
    Having problem with undefined in pop-up box
    AND
    attributes in line 158 is not defined :(

--- js/main.js
+++ js/main.js
@@ -5,11 +5,6 @@
 //GOAL: Proportional symbols representing attribute values of mapped features
 //STEPS:
 //1. Create the Leaflet map--done (in createMap())
-//2. Import GeoJSON data--done (in getData())
-//3. Add circle markers for point features to the map--done (in AJAX callback)
-//4. Determine which attribute to visualize with proportional symbols
-//5. For each feature, determine its value for the selected attribute
-//6. Give each feature's circle marker a radius based on its attribute value
 
 //function to create the Leaflet map
 function createMap(){
@@ -28,43 +23,207 @@ function createMap(){
     getData(map);
 };
 
-//define function to retrieve the data and place it on the map
+//calculate the radius of each proportional symbol
+function calcPropRadius(attValue) {
+    //scale factor to adjust symbol size evenly
+    var scaleFactor = 10;
+    //area based on attribute value and scale factor
+    var area = attValue * scaleFactor;
+    //radius calculated based on area
+    var radius = Math.sqrt(area/Math.PI);
+
+    return radius;
+};
+
+//3. Add circle markers for point features to the map--done (in AJAX callback)
+//function to convert markers to circle markers
+function pointToLayer(feature, latlng, attributes){
+    //Step 4: Assign the current attribute based on the first index of the attributes array
+    var attribute = attributes[0];
+    //check
+    console.log(attribute);
+
+    //create marker options
+    var options = {
+        fillColor: "#ff7800",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    //5. For each feature, determine its value for the selected attribute
+    var attValue = Number(feature.properties[attribute]);
+
+    //6. Give each feature's circle marker a radius based on its attribute value
+    options.radius = calcPropRadius(attValue);
+
+    //create circle marker layer
+    var layer = L.circleMarker(latlng, options);
+    layer.bindPopup(popupContent);
+
+    //event listeners to open popup on hover
+    layer.on({
+        mouseover: function(){
+            this.openPopup();
+        },
+        mouseout: function(){
+            this.closePopup();
+        }
+    });
+
+    //build popup content string starting with country
+    var popupContent = "<p><b>Country:</b> " + feature.properties.Country + "</p>";
+
+    //add formatted attribute to popup content string
+    var year = attribute.split("_")[1];
+    popupContent += "<p><b>Internet Users in " + year + ":</b> " + feature.properties[attribute] + " %</p>";
+
+    //bind the popup to the circle marker
+    layer.bindPopup(popupContent);
+
+    //return the circle marker to the L.geoJson pointToLayer option
+    return layer;
+};
+
+//Add circle markers for point features to the map
+function createPropSymbols(data, map, attributes){
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {
+        pointToLayer: function(feature, latlng){
+            return pointToLayer(feature, latlng, attributes);
+        }
+    }).addTo(map);
+};
+
+//Step 2: Import GeoJSON data
 function getData(map){
     //load the data
-   $.ajax("data/InternetUsage.geojson", {
+    $.ajax("data/InternetUsage.geojson", {
         dataType: "json",
         success: function(response){
-            //create marker options
-            var geojsonMarkerOptions = {
-                radius: 8,
-                fillColor: "#ff7800",
-                color: "#000",
-                weight: 1,
-                opacity: 1,
-                fillOpacity: 0.8
-            };
-
-            //create a Leaflet GeoJSON layer for markers and add it to the map
-            L.geoJson(response, {
-                pointToLayer: function (feature, latlng){
-                    return L.circleMarker(latlng, geojsonMarkerOptions);
-                }
-            }).addTo(map);
+            //call function to create proportional symbols
+            createPropSymbols(reference, map);
         }
     });
 };
 
-//function to attach popups to each mapped feature
-function onEachFeature(feature, layer) {
-    //no property named popupContent; instead, create html string with all properties
-    var popupContent = "";
-    if (feature.properties) {
-        //loop to add feature property names and values to html string
-        for (var property in feature.properties){
-            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+
+//GOAL: Allow the user to sequence through the attributes and resymbolize the map 
+//   according to each attribute
+//STEPS:
+//1. Create slider widget
+//Step 1: Create new sequence controls
+function createSequenceControls(map){
+    //create range input element (slider)
+      $('#mydiv').append('<input class="range-slider" type="range">');
+
+    //Step 5: click listener for buttons
+   $('.skip').click(function(){
+        //get the old index value
+        var index = $('.range-slider').val();
+
+        //Step 6: increment or decrement depending on button clicked
+        if ($(this).attr('id') == 'forward'){
+            index++;
+            //Step 7: if past the last attribute, wrap around to first attribute
+            index = index > 24 ? 0 : index;
+        } else if ($(this).attr('id') == 'reverse'){
+            index--;
+            //Step 7: if past the first attribute, wrap around to last attribute
+            index = index < 0 ? 24 : index;
+        };
+
+        //Step 8: update slider
+        $('.range-slider').val(index);
+        console.log(index);
+        //Step 9: pass new attribute to update symbols
+        updatePropSymbols(map, attributes[index]);
+    });
+    //Step 5: input listener for slider
+    $('.range-slider').on('input', function(){
+        //Step 6: get the new index value
+        var index = $(this).val();
+        console.log(index);
+    });
+
+    //set slider attributes
+    $('.range-slider').attr({
+        max: 24,
+        min: 0,
+        value: 0,
+        step: 1
+    });
+    //Step 9: pass new attribute to update symbols
+    updatePropSymbols(map, attributes[index]);
+};
+
+//add skip buttons
+    $('#mydiv').append('<button class="skip" id="reverse">Reverse</button>');
+    $('#mydiv').append('<button class="skip" id="forward">Skip</button>');
+
+//Import GeoJSON data
+function getData(map){
+    //load the data
+    $.ajax("data/InternetUsage.geojson", {
+        dataType: "json",
+        success: function(response){
+            //create an attributes array
+            var attributes = processData(response);
+
+            createPropSymbols(response, map, attributes);
+            createSequenceControls(map, attributes);
+
         }
-        layer.bindPopup(popupContent);
+    });
+};
+
+//3. Create an array of the sequential attributes to keep track of their order
+function processData(data){
+    //empty array to hold attributes
+    var attributes = [];
+
+    //properties of the first feature in the dataset
+    var properties = data.features[0].properties;
+
+    //push each attribute name into attributes array
+    for (var attribute in properties){
+        //only take attributes with population values
+        if (attribute.indexOf("YR") > -1){
+            attributes.push(attribute);
+        };
     };
+
+    //check result
+    console.log(attributes);
+
+    return attributes;
+};
+
+//Step 10: Resize proportional symbols according to new attribute values
+function updatePropSymbols(map, attribute){
+    map.eachLayer(function(layer){
+       if (layer.feature && layer.feature.properties[attribute]){
+            //access feature properties
+            var props = layer.feature.properties;
+
+            //update each feature's radius based on new attribute values
+            var radius = calcPropRadius(props[attribute]);
+            layer.setRadius(radius);
+
+            //add country to popup content string
+            var popupContent = "<p><b>Country:</b> " + props.Country + "</p>";
+
+            //add formatted attribute to mydiv content string
+            var year = attribute.split("_")[1];
+            popupContent += "<p><b>Internet Users in " + year + ":</b> " + props[attribute] + " %</p>";
+
+            //replace the layer popup
+            layer.bindPopup(popupContent, {
+                offset: new L.Point(0,-radius)
+            });
+        };
+    });
 };
 
 $(document).ready(createMap);
